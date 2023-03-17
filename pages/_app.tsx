65232aea5a09af99f73bd8d8e024308b0774d8a1
Graham Bewley <grahambewley@gmail.com>
2023-03-17 14:26:27 -0400

    Remove reference to storeprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,13 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SnackbarProvider } from 'notistack';
-import { StoreProvider } from '../utils/store';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<SnackbarProvider
 			anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
 		>
-			<StoreProvider>
 				<Component {...pageProps} />
-			</StoreProvider>
 		</SnackbarProvider>
 	);
 }
